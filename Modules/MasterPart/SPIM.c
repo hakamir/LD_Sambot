0ebcff398b707115a103cf297e67fb0bc7941c85
MarieDonnuts <marie.donnet@laposte.net>
2018-03-16 16:01:07 +0100

    SPI maj Duchemin

--- Modules/MasterPart/SPIM.c
+++ Modules/MasterPart/SPIM.c
@@ -19,7 +19,48 @@
 //------------------------------------------------------------------------------
 void SPIM_init(void)
 {
-	if(CALBC1_1MHZ==0xFF || CALDCO_1MHZ==0xFF)
+	// Waste Time, waiting Slave SYNC
+    __delay_cycles(250);
+	
+	// SOFTWARE RESET - mode configuration
+    UCB0CTL0 = 0;
+    UCB0CTL1 = (0 + UCSWRST*1 );
+	
+	// clearing IFg /16.4.9/p447/SLAU144j
+    // set by setting UCSWRST just before
+    IFG2 &= ~(UCB0TXIFG | UCB0RXIFG);
+	
+	// Configuration SPI (voir slau144 p.445)
+    // UCCKPH = 0 -> Data changed on leading clock edges and sampled on trailing edges.
+    // UCCKPL = 0 -> Clock inactive state is low.
+    //   SPI Mode 0 :  UCCKPH * 1 | UCCKPL * 0
+    //   SPI Mode 1 :  UCCKPH * 0 | UCCKPL * 0  <--
+    //   SPI Mode 2 :  UCCKPH * 1 | UCCKPL * 1
+    //   SPI Mode 3 :  UCCKPH * 0 | UCCKPL * 1
+    // UCMSB  = 1 -> MSB premier
+    // UC7BIT = 0 -> 8 bits, 1 -> 7 bits
+    // UCMST  = 0 -> CLK by Master, 1 -> CLK by USCI bit CLK / p441/16.3.6
+    // UCMODE_x  x=0 -> 3-pin SPI,
+    //           x=1 -> 4-pin SPI UC0STE active high,
+    //           x=2 -> 4-pin SPI UC0STE active low,
+    //           x=3 -> i²c.
+    // UCSYNC = 1 -> Mode synchrone (SPI)
+    UCB0CTL0 |= ( UCMST | UCMODE_0 | UCSYNC );
+    UCB0CTL0 &= ~( UCCKPH | UCCKPL | UCMSB | UC7BIT );
+    UCB0CTL1 |= UCSSEL_2;
+	
+	UCB0BR0 = 0x0A;     // divide SMCLK by 10
+    UCB0BR1 = 0x00;
+	
+	// SPI : Fonctions secondaires
+    // MISO-1.6 MOSI-1.7 et CLK-1.5
+    // Ref. SLAS735G p48,49
+    P1SEL  |= ( SCK | DATA_OUT | DATA_IN);
+    P1SEL2 |= ( SCK | DATA_OUT | DATA_IN);
+
+    UCB0CTL1 &= ~UCSWRST; 
+	
+	i/*f(CALBC1_1MHZ==0xFF || CALDCO_1MHZ==0xFF)
 	{
 		__bis_SR_register(LPM4_bits);
 	}
@@ -45,7 +86,7 @@ void SPIM_init(void)
 	
 	UCA0CTL1 = 0x00;
 	
-	P1OUT &= ~BIT5;
+	P1OUT &= ~BIT5;*/
 }
 
 //------------------------------------------------------------------------------
@@ -56,8 +97,9 @@ void SPIM_init(void)
 //------------------------------------------------------------------------------
 void SPIM_Tx(unsigned char c)
 {
+	while ((UCB0STAT & UCBUSY));   // attend que USCI_SPI soit dispo.
 	while(!(IFG2 & UCA0TXIFG));
-	UCA0TXBUF = c;
+	UCA0TXBUF = c;				   // Put character in transmit buffer
 }
 
 
@@ -69,6 +111,7 @@ void SPIM_Tx(unsigned char c)
 //------------------------------------------------------------------------------
 unsigned char SPIM_Rx(void)
 {
+	while( (UCB0STAT & UCBUSY) && !(UCB0STAT & UCOE) );
 	while(!(IFG2 & UCA0RXIFG));
 	return UCA0RXBUF;
 }
--- Modules/SlavePart/SPIS.c
+++ Modules/SlavePart/SPIS.c
@@ -19,15 +19,30 @@
 //------------------------------------------------------------------------------
 void SPIS_init(void)
 {
-	WDTCTL = WDTPW + WDTHOLD;             // Stop watchdog timer
 	P1OUT =  BIT4;                        // P1.4 set, else reset
 	P1REN |= BIT4;                        // P1.4 pullup
 	P1DIR = BIT0;                         // P1.0 output, else input
-	USICTL0 |= USIPE7 + USIPE6 + USIPE5 + USIOE; // Port, SPI slave
-	USICTL1 |= USIIE;           // Counter interrupt, flag remains set
-	USICTL0 &= ~USISWRST;                 // USI released for operation
-	USISRL = P1IN;                        // init-load data
-	USICNT = 8;
+	
+	// USI Config. for SPI 3 wires Slave Op.
+    // P1SEL Ref. p41,42 SLAS694J used by USIPEx
+    USICTL0 |= USISWRST;
+    USICTL1 = 0;		           
+	
+	// 3 wire, mode Clk&Ph / 14.2.3 p400
+    // SDI-SDO-SCLK - LSB First - Output Enable - Transp. Latch
+    USICTL0 |= (USIPE7 | USIPE6 | USIPE5 | USILSB | USIOE | USIGE );
+    // Slave Mode SLAU144J 14.2.3.2 p400
+    USICTL0 &= ~(USIMST);
+    USICTL1 |= USIIE;
+    USICTL1 &= ~(USICKPH | USII2C);
+
+    USICKCTL = 0; 			// No Clk Src in slave mode
+    USICKCTL &= ~(USICKPL | USISWCLK);	// Polarity - Input ClkLow
+
+    USICNT = 0;
+    USICNT &= ~(USI16B | USIIFGCC ); // Only lower 8 bits used 14.2.3.3 p 401 slau144j
+    USISRL = 0x23;					// hash, just mean ready; USISRL Vs USIR by ~USI16B set to 0
+    USICNT = 0x08;
 }
 
 //------------------------------------------------------------------------------
