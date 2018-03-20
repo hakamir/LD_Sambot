817d407c1d639d391d0f881dee6d937c61ec0856
MarieDonnuts <marie.donnet@laposte.net>
2018-03-20 09:25:40 +0100

    ajout test spi

---
+++ Modules/test_SPI_2231/Debug/SPI_2231.pp
@@ -0,0 +1,15 @@
+# FIXED
+
+SPI_2231.obj: ../SPI_2231.c
+SPI_2231.obj: C:/ti/ccsv6/ccs_base/msp430/include/msp430.h
+SPI_2231.obj: C:/ti/ccsv6/ccs_base/msp430/include/msp430g2231.h
+SPI_2231.obj: C:/ti/ccsv6/ccs_base/msp430/include/in430.h
+SPI_2231.obj: C:/ti/ccsv6/tools/compiler/ti-cgt-msp430_17.9.0.STS/include/intrinsics.h
+SPI_2231.obj: C:/ti/ccsv6/tools/compiler/ti-cgt-msp430_17.9.0.STS/include/intrinsics_legacy_undefs.h
+
+../SPI_2231.c: 
+C:/ti/ccsv6/ccs_base/msp430/include/msp430.h: 
+C:/ti/ccsv6/ccs_base/msp430/include/msp430g2231.h: 
+C:/ti/ccsv6/ccs_base/msp430/include/in430.h: 
+C:/ti/ccsv6/tools/compiler/ti-cgt-msp430_17.9.0.STS/include/intrinsics.h: 
+C:/ti/ccsv6/tools/compiler/ti-cgt-msp430_17.9.0.STS/include/intrinsics_legacy_undefs.h: 
